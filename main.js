f322169e6c4bc7afc710f3d69f232f096f2edd1f
meiya <meiya90hou@163.com>
2017-12-23 22:03:27 +0800

    简单轮播

---
+++ main.js
@@ -0,0 +1,25 @@
+$(p1).on('click',function () {
+        $(image).css({
+            transform:'translateX(0)'
+        })
+    });
+
+$(p2).on('click',function () {
+    $(image).css({
+        transform:'translateX(-400px)'
+    })
+});
+
+$(p3).on('click',function () {
+    $(image).css({
+        transform:'translateX(-800px)'
+    })
+});
+
+
+
+
+
+
+
+
